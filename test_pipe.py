a9a9f67d676cdb000986810186b7a399b101204e
Aber <me@abersheeran.com>
2021-01-19 14:06:05 +0800

    Version 2.0.0: Allow FF to deconstruct any iterable object, not only tuple.

--- test_pipe.py
+++ test_pipe.py
@@ -7,5 +7,6 @@ assert range(10) | F(filter, lambda x: x % 2) | F(sum) == 25
 assert (1, 2, 3) | F(sum) == 6
 
 assert (1, 2) | FF(lambda x, y: x + y) == 3
+assert [1, 2, 3] | FF(lambda x, y, z: x + y + z) == 6
 
 assert range(10) | F(reduce, lambda x, y: x + y) == 45
