df5b0db50488bf1675eb0832d5f58610f4cefd73
Aber <me@abersheeran.com>
2021-01-27 20:47:36 +0800

    Fix typo

--- pipe.py
+++ pipe.py
@@ -5,7 +5,7 @@ class F(partial):
     """
     Python Pipe. e.g.`range(10) | F(filter, lambda x: x % 2) | F(sum)`
 
-    WRANING: There will be a small performance loss when building a
+    WARNING: There will be a small performance loss when building a
     pipeline. Please do not use it in performance-sensitive locations.
     """
 
@@ -17,7 +17,7 @@ class FF(partial):
     """
     Python Pipe. e.g.`("f", 10) | FF(lambda letter, num: letter * num)`
 
-    WRANING: There will be a small performance loss when building a
+    WARNING: There will be a small performance loss when building a
     pipeline. Please do not use it in performance-sensitive locations.
     """
 
